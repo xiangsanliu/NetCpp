22ef92a7e0d1eee6b75ac8894c91df63ce6315a5
xiangsanliu <xiangjianjian@hotmail.com>
2017-12-21 11:55:34 +0800

    开始写ftp

---
+++ ftp_server.h
@@ -0,0 +1,18 @@
+//
+// Created by xiang on 2017/12/20.
+//
+
+#ifndef NETCPP_FTP_SERVER_H
+#define NETCPP_FTP_SERVER_H
+
+#include <winsock2.h>
+#include <iostream>
+#include <iptypes.h>
+#include <Iphlpapi.h>
+#include <tool.h>
+
+void doFtp() {
+
+}
+
+#endif //NETCPP_FTP_SERVER_H
